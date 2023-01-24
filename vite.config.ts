adef4078a71d85b1822dac99a87548178c3f9ace
Li Xu <li.xu@databricks.com>
2023-01-23 19:40:09 -0800

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./setupTests.js",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis", //<-- AWS SDK
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
   },
 });
